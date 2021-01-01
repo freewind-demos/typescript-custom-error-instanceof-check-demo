c39f5f8f94bd3cebf2b26b7a28c2b72881b3b86b
Freewind <nowindlee@gmail.com>
2021-01-02 00:18:44 +0800

    typescript-custom-error-instanceof-check-demo

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
-TypeScript Hello World Demo
-===========================
+TypeScript Custom Error "instanceof" Check Demo
+===============================================
+
+当typescript的编译目标为es5时，通过`extend Error`生成的自定义Error不能被instanceof正确识别。 如果是新一些的目标，比如es6，则无此问题。
+
+如果使用babel，可能有一些插件可以帮忙处理，但使用普通typescript编译器时要麻烦一些。
+
+有一种做法是在自定义的Error里增加下面这句：
+
+```
+Object.setPrototypeOf(this, new.target.prototype);
+```
+
+这样可以让instanceof正确识别。
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,28 @@
-console.log('Hello');
+abstract class Es5BaseError extends Error {
+  protected constructor() {
+    super();
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+class MyError1 extends Es5BaseError {
+  constructor() {
+    super();
+  }
+}
+
+class MyError2 extends Es5BaseError {
+  constructor() {
+    super();
+  }
+}
+
+
+try {
+  throw new MyError1();
+} catch (error) {
+  console.log("### error instanceof Error:", error instanceof Error);
+  console.log("### error instanceof Es5BaseError:", error instanceof Es5BaseError);
+  console.log("### error instanceof MyError1:", error instanceof MyError1);
+  console.log("### error instanceof MyError2:", error instanceof MyError2);
+}
